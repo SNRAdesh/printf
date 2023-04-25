9e25b524f1f48321f96a5d7019be340d677d6eb9
SNRAdesh <adesinatoheeb7@gmail.com>
2023-04-25 05:06:53 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,9 +1,7 @@
-
-
+#include "main.h"
 /**
  * _printf - formatted output conversion and print data.
  * @format: input string.
- *
  * Return: number of chars printed.
  */
 int _printf(const char *format, ...)
